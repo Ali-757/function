d227251c51752666c9ae3ea90498e3de04371e31
Abduvoitov <akbarabduvoitov7@gmail.com>
2022-09-29 10:24:41 +0500

    firs commit

---
+++ script.js
@@ -0,0 +1,60 @@
+// let fill = [];
+// let res = []
+// function generate(n) {
+//     for (var i = 0; i <= n; i++) {       
+//         console.log(i);
+//         res.push(i)
+//     }
+//     return res;
+// }
+
+// console.log(generate(100));
+
+
+
+
+
+
+
+// function nam(a) {
+// let sum = 0;
+// for (let i =0; i < a.length; i++){
+//     if (a[i] < 0){
+//         sum += a[i]
+//     }
+// }
+// console.log(sum);
+// }
+// nam([4, -5, -10, 6])
+
+
+
+
+
+// function negativs(params, son) {
+//     const resolt = params.concat(son)
+//     let natija = 0
+//     for (let i = 0; i < resolt.length; i++) {
+//         natija += resolt[i]
+//     }
+//     console.log(natija);
+//  }
+
+// negativs([4, -5, -10, 6], [6, 9, 14, -1, 6])
+
+
+
+
+
+
+// function convert(ozgar) {
+//     if (ozgar) {
+//         console.log("yes")
+//     }
+//     else{
+//         console.log("No");
+//     }
+// }
+
+
+// convert(true);
